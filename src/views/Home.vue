f07f078619ad73b57fc2309659ba726f659c34ce
k-cormack <k.cormack@gmail.com>
2018-11-07 11:26:07 -0700

    Added initial FrontPage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FrontPage/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+import FrontPage from '@/components/FrontPage.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    FrontPage,
   }
 }
 </script>
